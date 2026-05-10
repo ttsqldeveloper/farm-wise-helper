d30503f73dec95402425a4290d2398e0fb9915e3
teboho moloi <125126846+ttsqldeveloper@users.noreply.github.com>
2026-05-10 14:13:52 +0200

    Update again vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,9 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig();
+export default defineConfig({ 
+  vite: {
+    // any extra vite config here
+  },
+  target: 'static'
+});
